3829d1be275f6331a1e8835368f68e50f8980edc
vincenthirtz <31092476+vincenthirtz@users.noreply.github.com>
2025-11-12 12:05:41 +0100

    good head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 import '../styles/globals.css';
 import Navbar from '../components/Navbar/navbar';
 import Footer from '../components/Footer/footer';
 import BackToTopButton from '../components/Buttons/BackToTopButton';
 import { AppProps } from 'next/app';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+       <Head>
+        {/* Simple Analytics */}
+        <script
+          async
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+        ></script>
+        <noscript>
+          <img
+            src="https://queue.simpleanalyticscdn.com/noscript.gif"
+            alt=""
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
+      </Head>
+
       <Navbar />
       <Component {...pageProps} />
       <Footer />
