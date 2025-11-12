dde8fbc268f4397f24acefb53b294f78c6173119
vincenthirtz <31092476+vincenthirtz@users.noreply.github.com>
2025-11-12 13:14:58 +0100

    delete code

--- cypress/support/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    getTestData(dataTestAttribute: string): Chainable<JQuery<HTMLElement>>;
-  }
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,26 +4,10 @@ import Navbar from '../components/Navbar/navbar';
 import Footer from '../components/Footer/footer';
 import BackToTopButton from '../components/Buttons/BackToTopButton';
 import { AppProps } from 'next/app';
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-       <Head>
-        {/* Simple Analytics */}
-        <script
-          async
-          src="https://scripts.simpleanalyticscdn.com/latest.js"
-        ></script>
-        <noscript>
-          <img
-            src="https://queue.simpleanalyticscdn.com/noscript.gif"
-            alt=""
-            referrerPolicy="no-referrer-when-downgrade"
-          />
-        </noscript>
-      </Head>
-
       <Navbar />
       <Component {...pageProps} />
       <Footer />
