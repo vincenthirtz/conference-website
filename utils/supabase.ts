ad225621a868e9de578fb2ac41d8faf647a69219
Alicia <Alicia@MacBook-Pro-de-Alicia.local>
2025-12-31 08:33:30 +0100

    update vars

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -14,7 +14,9 @@ import type {
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const SUPABASE_SERVICE_ROLE = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY;
+const SUPABASE_SERVICE_ROLE =
+  process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY ||
+  process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 /* -----------------------------------------------------------
  * Sanity checks env
